09e38795b55e228e8ba13f6a10c7b86c10cb8c31
mokos <Hidenobu@mac-himari.local>
2013-10-21 20:34:49 +0900

    b

--- Deferred.hx
+++ Deferred.hx
@@ -1,5 +1,7 @@
 package ;
 
+using Lambda;
+
 enum DeferredState<T, E> {
 	Pending;
 	Resolved(result:T);
@@ -79,23 +81,23 @@ class PromiseWithErrorType<T, E> {
 	}
 
 	public static function immediate<U>(x : U) : Promise<U> {
-		var d = new Deferred<U>();
-		d.resolve(x);
-		return d.promise();
+		return (new Deferred<U>()).resolve(x);
 	}
 }
 
 class DeferredWithErrorType<T, E> extends PromiseWithErrorType<T, E> {
-	public function promise() : PromiseWithErrorType<T, E> {
+	public function promise() : P<T, E> {
 		return this;
 	}
 
-	public function resolve(result : T) : Void {
+	public function resolve(result : T) : P<T, E> {
 		fix(Resolved(result));
+		return this;
 	}
 
-	public function reject(error : E) : Void {
+	public function reject(error : E) : P<T, E> {
 		fix(Rejected(error));
+		return this;
 	}
 
 	function fix(s : DeferredState<T, E>) : Void {
@@ -120,31 +122,30 @@ class DeferredWithErrorType<T, E> extends PromiseWithErrorType<T, E> {
 		resetQueue();
 	}
  
-	public static function when<A, B, C, D, E>(
-		 p1 : Promise<A>,
-		 p2 : Promise<B>,
-		?p3 : Promise<C>,
-		?p4 : Promise<D>,
-		?p5 : Promise<E>
+	public static function when<A, B, C, D, E, Er>(
+		 p1 : P<A, Er>,
+		 p2 : P<B, Er>,
+		?p3 : P<C, Er>,
+		?p4 : P<D, Er>,
+		?p5 : P<E, Er>
 	)
 	{
 		var d = new Deferred<ReturnTuple<A,B,C,D,E>>();
+		var promises:Array<P<Dynamic, Er>> = [p1, p2, p3, p4, p5];
+		promises = promises.filter(function(x) return x!=null);
+
+		// results
+		var r:Array<Dynamic> = [for (p in promises) null];
+
+
 		var doneNum = 0;
-		var ps:Array<Promise<Dynamic>> = [p1, p2];
-		if (p3!=null) ps.push(p3);
-		if (p4!=null) ps.push(p4);
-		if (p5!=null) ps.push(p5);
-		var promiseNum = ps.length;
-
-		var r:Array<Dynamic> = [for (i in 0...promiseNum) null];
-
-		for (i in 0...ps.length) {
-			var p = ps[i];
+		for (i in 0...promises.length) {
+			var p = promises[i];
 
 			p.done(function(result) {
 				r[i] = result;
-				if (++doneNum==promiseNum) {
-					switch (promiseNum) {
+				if (++doneNum==promises.length) {
+					switch (promises.length) {
 					case 2: d.resolve(Ret2(r[0], r[1]));
 					case 3: d.resolve(Ret3(r[0], r[1], r[2]));
 					case 4: d.resolve(Ret4(r[0], r[1], r[2], r[3]));
